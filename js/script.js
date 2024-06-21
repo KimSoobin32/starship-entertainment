d9ce4a4be1f7c38b7cc453d0dcbb00b593e90405
ksb <jwe06120@gmail.com>
2024-06-21 10:29:09 +0900

    아티스트 슬라이더 정리

--- js/script.js
+++ js/script.js
@@ -11,42 +11,15 @@ $(function () {
     },
   });
 
-  // const artistSlider = new Swiper('.artist-slider', {
-  //   // autoplay: { delay: 1000 },
-  //   loop: true,
-  //   centeredSlides: true,
-  //   // slidesPerView: '7',
-  //   // spaceBetween: 20,
-
-  //   slidesPerView: '7',
-  //   // spaceBetween: 10, // 슬라이드 사이의 간격 조정
-  //   // navigation: {
-  //   //   nextEl: '.btn-next',
-  //   //   prevEl: '.btn-prev',
-  //   // },
-  // });
-
-  // // 슬라이드 클릭 이벤트 추가
-  // $('.artist-slider .swiper-slide').on('click', function () {
-  //   const index = $(this).index();
-  //   //artistSlider.slideToLoop(index, 400); // 클릭한 슬라이드로 이동, 400ms 동안 이동
-  //   artistSlider.slideTo(index, 400); // 클릭한 슬라이드로 이동, 400ms 동안 이동
-  // });
-
   const artistSlider = new Swiper('.artist-slider', {
+    // autoplay: { delay: 5000 },
     loop: true,
     centeredSlides: true,
-    slidesPerView: 7,
-    spaceBetween: 10,
+    slidesPerView: '5',
+    // spaceBetween: 10,
     navigation: {
       nextEl: '.btn-next',
       prevEl: '.btn-prev',
     },
-  });
-
-  // 슬라이드 클릭 이벤트 추가
-  $('.artist-slider .swiper-slide').on('click', function () {
-    const realIndex = $(this).data('swiper-slide-index');
-    artistSlider.slideToLoop(realIndex, 400); // 클릭한 슬라이드로 이동, 400ms 동안 이동
   });
 });
